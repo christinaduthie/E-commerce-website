f23af99237e475dab1c7eb0899c5ce572699a777
christinaduthie <christinarduthie@gmail.com>
2023-05-24 22:32:50 +0530

    product page

---
+++ src/components/UI/ProductCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProductCard = () => {
+    return (
+        <div className='product__item'>
+            <div className='product_img'>
+                <img src='' alt=''></img>
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard
