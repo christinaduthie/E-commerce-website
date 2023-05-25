7fea351126331a26f30fd7bc9170e04309173ce2
christinaduthie <christinarduthie@gmail.com>
2023-05-25 22:43:18 +0530

    home page products

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -3,20 +3,23 @@ import productImg from '../../assets/images/leadership-book-01.jpg'
 import { motion } from 'framer-motion';
 import '../../styles/product-card.css';
 import { Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const ProductCard = () => {
     return (
         <Col lg='3' md='4'>
             <div className='product__item'>
                 <div className='product_img'>
-                    <img src={productImg} alt=''></img> </div>
+                    <motion.img whileHover={{ scale: 0.9 }} src={productImg} alt='' />
+                </div>
                 <div className='p-2 product__info'>
-                    <h3 className='product__name'>Leaders Eat Last</h3>
+                    <h3 className='product__name'><Link to='/shop/id'>Leaders Eat Last</Link></h3>
                     <span>Self Help</span>
                 </div>
                 <div className='product__card-bottom d-flex align-items-center justify-content-between p-2'>
                     <span className='price'>$1.4</span>
-                    <span><i class="ri-add-line"></i></span>
+                    <motion.span whileTap={{ scale: 1.2 }}><i class="ri-add-line"></i>
+                    </motion.span>
                 </div>
             </div>
         </Col>
