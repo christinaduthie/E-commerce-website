cb26c34ed51928e7810c2fc5a01a252d828a10a2
christinaduthie <christinarduthie@gmail.com>
2024-03-12 22:12:53 -0500

    Routing pages

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
+import Cart from '../pages/Cart'
+import Checkout from '../pages/Checkout'
+import Login from '../pages/Login'
+import ProductDetails from '../pages/ProductDetails'
+import Shop from '../pages/Shop'
+import Signup from '../pages/Signup'
 
 const Routers = () => {
-  return (
-    <div>Routers</div>
-  )
+  return <Routes>
+    <Route path='home' element={<Home/>}/>
+    <Route path='shop' element={<Shop/>}/>
+    <Route path='shop/:id' element={<ProductDetails/>}/>
+    <Route path='cart' element={<Cart/>}/>
+    <Route path='checkout' element={<Checkout/>}/>
+    <Route path='login' element={<Login/>}/>
+    <Route path='signup' element={<Signup/>}/>
+  </Routes>
 }
 
 export default Routers
